f5e528cee1992b2745be7ddba6bcd85d0668e4a5
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2024-03-14 23:06:18 +0545

    feat: APIResponse helper class

---
+++ app/Libs/APIResponse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Libs;
+
+use Illuminate\Http\JsonResponse;
+
+class APIResponse
+{
+    /**
+     * success response method.
+     *
+     * @param mix $data
+     * @param string $message
+     * @return void
+     */
+    public static function success($data = null, $message = null): JsonResponse
+    {
+        $result = [
+            'success' => true
+        ];
+        if (!empty($data)) {
+            $result['data'] = $data;
+        }
+        if ($message) {
+            $result['message'] = $message;
+        }
+        return response()->json($result);
+    }
+
+    /**
+     * error response method.
+     *
+     * @param mix $data
+     * @param string $message
+     * @return void
+     */
+    public static function error($data = null, $message = null, $code = 500): JsonResponse
+    {
+        $result = [
+            'success' => false
+        ];
+        if (!empty($data)) {
+            $result['data'] = $data;
+        }
+        if ($message) {
+            $result['message'] = $message;
+        }
+        return response()->json($result, $code);
+    }
+}
